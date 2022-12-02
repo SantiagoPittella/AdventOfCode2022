78efa6968218adb90fc963949de1e6f8a1dbe424
SantiagoPittella <pittellasantiago@gmail.com>
2022-12-02 18:15:48 -0300

    add day1

---
+++ day1.py
@@ -0,0 +1,35 @@
+def part_a():
+    with open("day1.input") as f:
+        max_sum = 0
+        actual_sum = 0
+        for line in f:
+            line = line.strip()
+            if line == "":
+                if actual_sum > max_sum:
+                    max_sum = actual_sum
+                actual_sum = 0
+            else:
+                actual_sum += int(line)
+        print(f"max sum: {max_sum}")
+        print(f"actual_sum: {actual_sum}")
+
+part_a()
+
+def part_b():
+    with open("day1.input") as f:
+        sums = []
+        actual_sum = 0
+        for line in f:
+            line = line.strip()
+            if line == "":
+                sums.append(actual_sum)
+                actual_sum = 0
+            else:
+                actual_sum += int(line)
+        print(f"sums: {sums}")
+        sums.sort()
+        sums.reverse()
+        print(f"sums sorted: {sums}")
+        print(f"max 3 sum: {sum(sums[:3])}")
+
+part_b()
